edfe53197e5d17461d6281de38f01a378c1b950c
MaksimZ91 <ak4202@mail.ru>
2020-11-04 09:36:41 +0800

    12

---
+++ 14-5zadanie.js
@@ -0,0 +1,15 @@
+ const clear = document.querySelector('.clear')
+ clear.addEventListener('click',() => {localStorage.clear()}) 
+ let name=""
+ let date= ""
+ if (localStorage.getItem('myName')){
+      alert(`«Добрый день, ${localStorage.getItem('myName')}! Давно не виделись. В последний раз вы были у нас ${localStorage.getItem('date')}»`)
+      date=new Date()
+      localStorage.setItem('date',date)
+ }else{
+    name=prompt(`«Добро пожаловать! Назовите, пожалуйста, ваше имя ${name}»` )
+    date=new Date()
+    localStorage.setItem('myName',name)
+    localStorage.setItem('date',date)
+         }
+
