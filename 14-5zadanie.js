c3c6f7bff12107b398d9edfc4f6b4b7640b2e6ee
MaksimZ91 <ak4202@mail.ru>
2020-11-04 10:13:28 +0800

    comp

--- 14-5zadanie.js
+++ 14-5zadanie.js
@@ -5,11 +5,11 @@
  if (localStorage.getItem('myName')){
       alert(`«Добрый день, ${localStorage.getItem('myName')}! Давно не виделись. В последний раз вы были у нас ${localStorage.getItem('date')}»`)
       date=new Date()
-      localStorage.setItem('date',date)
+      localStorage.setItem('date',date.toLocaleString())
  }else{
     name=prompt(`«Добро пожаловать! Назовите, пожалуйста, ваше имя ${name}»` )
     date=new Date()
     localStorage.setItem('myName',name)
-    localStorage.setItem('date',date)
+    localStorage.setItem('date',date.toLocaleString())
          }
 
