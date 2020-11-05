7392ecef0066b835f5c597731ab61dcba6e391ad
MaksimZ91 <ak4202@mail.ru>
2020-11-05 17:59:45 +0800

    in progress

--- 8zadanie/8zadanie.js
+++ 8zadanie/8zadanie.js
@@ -3,24 +3,34 @@ const inputlimitNode=document.querySelector('.limit');
 const btnNode=document.querySelector('.btn');
 const listNode=document.querySelector('.list');
 const errLimitNode=document.querySelector('.errorLimit');
+const clear = document.querySelector('.clear')
+clear.addEventListener('click',() => {localStorage.clear()}) 
 
 btnNode.addEventListener('click',shortNum);
 btnNode.addEventListener('click', clearTable)
-
+saveImg()
 function useRequest (){
   fetch(` https://picsum.photos/v2/list?page=${+inputPageNode.value}&limit=${+inputlimitNode.value}`)
   .then((response) => { return response.json(); })
   .then((data) => {addResult(data)})
   .catch(() => {console.log("error")});
  }
-
- function addResult (data){
-  for (let key in data){
-   errLimitNode.setAttribute('hidden', 'true') 
-   listNode.insertAdjacentHTML("afterbegin",`<li><img src="${data[key].download_url}" alt="${data[key].author}"width="300" height="200" ></li>`)
- }   
+ function saveImg() {
+   if (localStorage.getItem('data')){
+    let object = JSON.parse(localStorage.getItem('data'))
+  for (let key in object){
+    errLimitNode.setAttribute('hidden', 'true')
+    listNode.insertAdjacentHTML("afterbegin",`<li><img src="${object[key].download_url}" alt="${object[key].author}"width="300" height="200" ></li>`)
+  }}return
 }
 
+ function addResult (data){
+  localStorage.setItem('data',JSON.stringify(data))
+  for (let key in data){
+   errLimitNode.setAttribute('hidden', 'true')
+   listNode.insertAdjacentHTML("afterbegin",`<li><img src="${data[key].download_url}" alt="${data[key].author}"width="300" height="200" ></li>`)
+  }   
+}
 function shortNum(){
   let myPromise = new Promise((resolve, reject) => {
     if (comCondlimit()&&comCondPage()){
@@ -64,7 +74,7 @@ function  clearTable (){
   if (list.length==0){
     return
   }else{
-    list.forEach((element,index)=>{
+    list.forEach((element)=>{
       listNode.removeChild(element)
     })
   }
